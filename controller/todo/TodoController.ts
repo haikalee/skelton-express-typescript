33ae66013e2211ee8391b7aff3dd607733943aa4
Mochammad Haikal <haikal@venturology.id>
2023-03-11 08:13:14 +0700

    error todo model on todo controller

--- controller/todo/TodoController.ts
+++ controller/todo/TodoController.ts
@@ -5,10 +5,16 @@ import Todo from "../../model/TodoModel";
 const TodoModel = new Todo()
 
 export default class TodoController extends Controller {
+    todoModel: Todo
+    constructor() {
+        super()
+        this.todoModel = new Todo()
+    }
+
     list(req: Request, res: Response) {
         // get todos
-        // TodoModel.list.then(data => {
-        //     console.log(data)
-        // })
+        TodoModel.list.then(data => {
+            console.log(data)
+        })
     }
 }
